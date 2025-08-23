019ee7d4cddb74bbf651bda163571366622cffda
unknown <lmpellowski@gmail.com>
2025-08-22 21:00:40 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let autocomplete;
+
+function initAutocomplete() {
+// Attach autocomplete to the input field
+autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('autocomplete'),
+    {
+    types: ['address'], // only addresses
+    componentRestrictions: { country: "us" }, // optional: restrict to a country
+    fields: ["formatted_address", "geometry", "name"] // what data to return
+    }
+);
+
+// When the user selects an address
+autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    console.log("Selected place:", place.formatted_address);
+    // You can fill hidden fields with place data if needed
+});
+}
+
+// Initialize after script load
+window.onload = initAutocomplete;
